6198a44e5377fa78e5d02baa4638acab273f86f3
Nikos Avgoustis <40135168+NikosAvg@users.noreply.github.com>
2026-06-12 10:10:23 +0300

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,82 @@
 const std = @import("std");
-const aoc = @import("../aoc.zig");
+const aoc = @import("aoc.zig");
 
-pub fn solve(
-    allocator: std.mem.Allocator,
-    input: []const u8,
-) !void {
-    _ = allocator;
-
-    var it = std.mem.splitScalar(
-        u8,
-        input,
-        '\n',
-    );
-
-    while (it.next()) |line| {
-        if (line.len == 0)
-            continue;
-
-        std.debug.print(
-            "{s}\n",
-            .{line},
-        );
-    }
-}
+const day01 = @import("days/day01.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa =
+        std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    const allocator = gpa.allocator();
+    const allocator =
+        gpa.allocator();
+
+    var args =
+        std.process.args();
+
+    _ = args.next();
+
+    const day_arg =
+        args.next() orelse {
+        std.debug.print(
+            "Usage: zig build run -- 1\n",
+            .{},
+        );
+        return;
+    };
+
+    const day =
+        try std.fmt.parseInt(
+            u8,
+            day_arg,
+            10,
+        );
 
     const input =
-        try aoc.readFileAlloc(
+        try aoc.readInput(
             allocator,
-            "input/day01.txt",
+            day,
         );
     defer allocator.free(input);
 
-    const t = aoc.timer();
+    switch (day) {
+        1 => {
+            const timer =
+                aoc.startTimer();
 
-    try solve(
-        allocator,
-        input,
-    );
+            const p1 =
+                try day01.part1(
+                    allocator,
+                    input,
+                );
 
-    std.debug.print(
-        "Elapsed: {} us\n",
-        .{t.read() / 1000},
-    );
+            const p2 =
+                try day01.part2(
+                    allocator,
+                    input,
+                );
+
+            aoc.printResult(
+                1,
+                p1,
+            );
+
+            aoc.printResult(
+                2,
+                p2,
+            );
+
+            std.debug.print(
+                "Time: {} μs\n",
+                .{timer.read() / 1000},
+            );
+        },
+
+        else => {
+            std.debug.print(
+                "Day not implemented\n",
+                .{},
+            );
+        },
+    }
 }
