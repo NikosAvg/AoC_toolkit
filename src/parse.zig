8ef2b16687a34fd56a50870fcaedefaa4fc78a4d
Nikos Avgoustis <40135168+NikosAvg@users.noreply.github.com>
2026-06-12 10:09:32 +0300

    Update parse.zig

--- src/parse.zig
+++ src/parse.zig
@@ -1,35 +1,22 @@
 const std = @import("std");
 
-pub fn parseInt(
+pub fn int(
     comptime T: type,
-    s: []const u8,
+    text: []const u8,
 ) !T {
     return std.fmt.parseInt(
         T,
-        std.mem.trim(u8, s, " "),
+        std.mem.trim(u8, text, " "),
         10,
     );
 }
 
-pub fn parseInts(
-    allocator: std.mem.Allocator,
-    line: []const u8,
-) !std.ArrayList(i64) {
-    var result = std.ArrayList(i64).init(
-        allocator,
-    );
-
-    var it = std.mem.tokenizeAny(
+pub fn lines(
+    text: []const u8,
+) std.mem.SplitIterator(u8, .scalar) {
+    return std.mem.splitScalar(
         u8,
-        line,
-        " ,",
+        text,
+        '\n',
     );
-
-    while (it.next()) |token| {
-        try result.append(
-            try parseInt(i64, token),
-        );
-    }
-
-    return result;
 }
