b8e76a8fc615a0816801dd1a62277b5967161318
Nikos Avgoustis <40135168+NikosAvg@users.noreply.github.com>
2026-06-12 10:13:46 +0300

    Create functions.zig
    Usefull everyday function and code snippets

---
+++ src/functions.zig
@@ -0,0 +1,33 @@
+pub fn HashSet(
+    comptime T: type,
+) type {
+    return std.AutoHashMap(
+        T,
+        void,
+    );
+}
+
+var map =
+    std.StringHashMap(u64)
+    .init(allocator);
+var pq =
+    std.PriorityQueue(
+        Node,
+        void,
+        Node.lessThan,
+    ).init(
+        allocator,
+        {},
+    );
+
+var queue =
+    std.ArrayList(Node)
+    .init(allocator);
+
+var head: usize = 0;
+
+while (head < queue.items.len) {
+    const current =
+        queue.items[head];
+    head += 1;
+}
